747de8dc7bf81620a08bc4cd52d5a1cac7b81676
erol1098 <erol1098@gmail.com>
2022-09-06 17:02:31 +0300

    Blog Models Added

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+  name = models.CharField(max_length=16, null=True)
+  
+  class Meta:
+    ordering = ['name']
+    verbose_name_plural = 'Categories'
+
+  def __str__(self):
+    return f"{self.name}"
+
+
+
 class Post(models.Model):
   title = models.CharField("Title", max_length=64)
   content = models.TextField("Content")
@@ -9,19 +21,13 @@ class Post(models.Model):
   last_updated = models.DateTimeField(auto_now=True)
   status = models.CharField("Status", max_length=2, null=True)
   slug = models.IntegerField(null=True)
+  category = models.ForeignKey(Category, on_delete=models.PROTECT)
 
   def __str__(self):
     return f"{self.title}"
 
   class Meta:
     ordering = ['publish_date']
-    verbose_name_plural = 'Post'
+  
 
 
-class Category(models.Model):
-  name = models.CharField(max_length=16)
-  post = models.ForeignKey(Post, on_delete=models.PROTECT)
-
-
-  def __str__(self):
-    return f"{self.name}"
