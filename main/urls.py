64b197ff29b05d5ad5a1d2649c7edf4b2f88c1e8
erol1098 <erol1098@gmail.com>
2022-09-07 19:41:42 +0300

    Login-Register Added

--- main/urls.py
+++ main/urls.py
@@ -19,6 +19,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
     path('', include("blog.urls")),
+    path('user/', include("users.urls")),
+    path('accounts/', include('django.contrib.auth.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+
+def user_login(request):
+    form = AuthenticationForm(request, data=request.POST)
+
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            # messages.success(request, "Login Successfull")
+            login(request,user)
+            return redirect('index')
+
+    return render(request, 'users/user_login.html', {"form":form})
+
+def register(request):
+    
+    if request.method == 'POST':
+      
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+      
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+          
+            user = authenticate(username=username, password=password)
+            
+        
+            login(request, user)
+         
+            return redirect('index')
+            
+    else:
+        form = UserCreationForm()
+    
+    ctx = {
+        'form': form
+    }
+    
+    return render(request, "users/register.html", ctx)
+
+
+def user_logout(request):
+    # messages.success(request,'You logged out!')
+    logout(request)
+    return redirect('index')
